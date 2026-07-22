32a7707e24f616c588ff1a0d12f2d2f8fb85e51f
lucassss9 <lucasgabriel09104@gmail.com>
2026-07-22 14:51:40 -0300

    initial commit

---
+++ src/main/java/com/cury/automacaocatraca/AutomacaoCatracaApplication.java
@@ -0,0 +1,13 @@
+package com.cury.automacaocatraca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoCatracaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoCatracaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cury/automacaocatraca/AutomacaoCatracaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cury.automacaocatraca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomacaoCatracaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
